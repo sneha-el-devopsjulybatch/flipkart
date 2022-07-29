07b40a58143462e486605c2e6884e27b51792c14
snehaK6271 <snehakedaresh6271>
2022-07-29 16:29:11 +0530

    updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 hello i am updating this line from master branch..
+<<<<<<< HEAD
 Updating this line from development branch..
+=======
+>>>>>>> 05274fc (updated)
 This file contains the deployment script..
 Updating in development branch..

289e1f22a310410aae36b894c2d2b044d3b8644d
snehaK6271 <snehakedaresh6271>
2022-07-29 16:00:30 +0530

    updated deploy file

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+Updating in uat branch..
+hello i am updating this line from master branch..
 Updating this line from development branch..
 This file contains the deployment script..
 Updating in development branch..
